e34fcf1cd3f14373d23368b586267476b945ac08
Jina-n11 <jinanqbh37@gmail.com>
2024-06-11 18:28:16 +0300

    add  solutions

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -21,3 +21,43 @@ after  double the size of the list
  maximum steps = log2(256)   => log2(2^8)   ==  2^8 = 256    so the maximum steps is  = 8
 */
 
+
+///  Give the run time for each of these scenarios in terms of Big O
+
+/*
+ * 1.3   You have a name, and you want to find the person’s phone number 
+in the phone book. 
+we have tow away to find the person’s phone number 
+in the phone book :
+
+   if the names is sorted 
+{
+       using  Binary Search O(log2 n)
+}
+else
+{
+     using simple search O(n)
+}
+
+ */
+
+
+
+/*
+ * 1.4   You have a phone number, and you want to find the person’s name 
+in the phone book. (Hint: You’ll have to search through the whole 
+book!)?
+            solution: Simple search O(n)
+ */
+
+
+/*
+ * 1.5   You want to read the numbers of every person in the phone book.
+ *           solution:   Simple search O(n)
+ */
+
+/*
+ * 1.6   You want to read the numbers of just the As.
+ * solution :  Simple search O(n)
+ */
+
