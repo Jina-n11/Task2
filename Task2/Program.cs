b32f43b8ee26793c903448499921c605b1027eb9
Jina-n11 <jinanqbh37@gmail.com>
2024-06-11 18:08:15 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+///TASK2 
+
+/*
+ * 1.1   Suppose you have a sorted list of 128 names, and you’re searching 
+through it using binary search. What’s the maximum number of 
+steps it would take?
+
+when we used the Binary search the maximum number of steps can be found using the logarithm function is (7).
+the maximum steps = log2(128)  => 2*2*2*2*2*2*2 = 128 == 2^7
+*/
+
+/*
+ *  1.2   Suppose you double the size of the list. What’s the maximum 
+number of steps now?
+
+For the initial list 128 names, we saw that the maximum steps were log2(128) ≈ 7 .
+
+after  double the size of the list
+128 * 2 = 256
+ maximum steps = log2(256)   => log2(2^8)   ==  2^8 = 256    so the maximum steps is  = 8
+*/
+
